2c3d71868dfa43d4ed56c695bcd122e5260a05c4
ratzupaltuff <quarksandstings.vw@gmail.com>
2018-04-28 19:38:43 +0200

    class improvements

--- Blinkenlights_ServerProcessing/.build/source/Blinkenlights_ServerProcessing.java
+++ Blinkenlights_ServerProcessing/.build/source/Blinkenlights_ServerProcessing.java
@@ -17,64 +17,18 @@ import java.io.IOException;
 public class Blinkenlights_ServerProcessing extends PApplet {
 
 
-// Processing UDP example to send and receive binary data
-// press f to toggel between LED flickering twice per second or not
-// the LED will illuminate when any other key is held down and
-// turn back off when the key is released.
 
 UDP udp; // define the UDP object
 
-//layout on wall
-String ip11 = "192.168.3.139";   String ip12 = "192.168.3.140";   String ip13 = "192.168.3.141";
-String ip21 = "192.168.3.142";   String ip22 = "192.168.3.143";   String ip23 = "192.168.3.144";
-String ip31 = "192.168.3.145";   String ip32 = "192.168.3.146";   String ip33 = "192.168.3.147";
-
-String[][] ipArray = {  {ip11,ip12,ip13},
-                         {ip21,ip22,ip23},
-                         {ip31,ip32,ip33}  };
-
-Light light11 = new Light(ip11);
-Light light12 = new Light(ip12);
-Light light13 = new Light(ip13);
-
-Light light21 = new Light(ip21);
-Light light22 = new Light(ip22);
-Light light23 = new Light(ip23);
-
-Light light31 = new Light(ip31);
-Light light32 = new Light(ip32);
-Light light33 = new Light(ip33);
-
-
 int port = 8881; // the destination port
-
-//Light l=new Light(ip11);
 
 long previousMillis = 0;
 int light = 0;
 long interval = 5000;
 int flicker = 0;
 int held = 0;
+
 boolean transferedsuccessful = false;
-
-//layout on sender
-/*
-String brighter = "0xF700FF";        String lower = "0xF7807F";            String off = "0xF740BF";           String on = "0xF7C03F";
-
-String red = "0xF720DF";             String green = "0xF7A05F";            String blue = "0xF7609F";          String white = "0xF7E01F";
-
-String lightred = "0xF710EF";        String lightgreen = "0xF7906F";       String lightblue = "0xF750AF";     String flash = "0xF7D02F";
-
-String orange = "0xF730CF";          String lightbluegreen = "0xF7B04F";   String violet = "0xF7708F";        String strobe = "0xF7F00F";
-
-String lightorange = "0xF708F7";     String darkerbluegreen = "0xF78877";  String lightviolet = "0xF748B7";   String fade = "0xF7C837";
-
-String yellow = "0xF728D7";          String darkbluegreen = "0xF7A857";    String pink = "0xF76897";          String smooth = "0xF7E817";
-*/
-//"red","orange","orangeyellowgreen","yellowgreen","yellow",
-    //                                              "green","greenblue","bluegreen","lightbluegreen","lightblue",
-    //                                              "blue","bluepurple","purpleblue","purple","lightpurple"
-
 
 Color red = new Color(0);
 Color orange = new Color(1);
@@ -96,7 +50,7 @@ Color lightpurple = new Color(14);
 
 Color white = new Color(15);
 
-
+LightMatrix lightMatrix = new LightMatrix();
 
 public void setup() {
   udp = new UDP( this, port); // create a new datagram connection on port 8888
@@ -117,7 +71,7 @@ public void draw() {  // draw() loops forever, until stopped
   for(int i=0;i <3; i++){
     for(int j=0;j <3; j++){
       fill(0,0,0);
-      text("IP= "+ ipArray[j][i], i*300+20, j*300+20);
+      text("IP= "+ lightMatrix.getLight(j+1,i+1).getIpAddr(), i*300+20, j*300+20);
     }
   }
 
@@ -161,7 +115,7 @@ public void receive( byte[] data, String ip, int port ) { // <– extended handl
 public void sendudp(String content){
         println("Tried to send: --> "+ content);
         while(!transferedsuccessful){
-          udp.send(content, ip11 , port);
+          //udp.send(content, ip11 , port);
           delay(1500);
         }
         transferedsuccessful = false;
@@ -273,6 +227,46 @@ class Light{
     public void changeColor(Color c){
         currentColor=c;
     }
+
+}
+class LightMatrix {
+  private int sizeX;
+  private int sizeY;
+
+  //layout on wall
+  String ip11 = "192.168.3.139";   String ip12 = "192.168.3.140";   String ip13 = "192.168.3.141";
+  String ip21 = "192.168.3.142";   String ip22 = "192.168.3.143";   String ip23 = "192.168.3.144";
+  String ip31 = "192.168.3.145";   String ip32 = "192.168.3.146";   String ip33 = "192.168.3.147";
+
+  /*String[][] ipArray = {  {ip11,ip12,ip13},
+                                 {ip21,ip22,ip23},
+                                 {ip31,ip32,ip33}  };*/
+
+  Light light11 = new Light(ip11);  Light light12 = new Light(ip12);  Light light13 = new Light(ip13);
+  Light light21 = new Light(ip21);  Light light22 = new Light(ip22);  Light light23 = new Light(ip23);
+  Light light31 = new Light(ip31);  Light light32 = new Light(ip32);  Light light33 = new Light(ip33);
+
+  Light[][] lights = {{light11,light12,light13},{light21,light22,light23},{light31,light32,light33}};
+
+  LightMatrix(){
+    this.sizeX=3;
+    this.sizeY=3;
+  }
+
+  public boolean setLightPos(Light light, int posX, int posY){
+      if(posX<=sizeX && posY<=sizeY){
+          lights[posX][posY]=light;
+          return true;
+      }
+      return false;
+  }
+
+  public Light getLight(int posX, int posY){
+      if(posX<=sizeX && posY<=sizeY){
+          return lights[posX-1][posY-1];
+      }
+      return new Light("127.0.0.1");
+  }
 
 }
   public void settings() {  size(1200,900); }
---
+++ Blinkenlights_ServerProcessing/LightMatrix.pde
@@ -0,0 +1,40 @@
+class LightMatrix {
+  private int sizeX;
+  private int sizeY;
+
+  //layout on wall
+  String ip11 = "192.168.3.139";   String ip12 = "192.168.3.140";   String ip13 = "192.168.3.141";
+  String ip21 = "192.168.3.142";   String ip22 = "192.168.3.143";   String ip23 = "192.168.3.144";
+  String ip31 = "192.168.3.145";   String ip32 = "192.168.3.146";   String ip33 = "192.168.3.147";
+
+  /*String[][] ipArray = {  {ip11,ip12,ip13},
+                                 {ip21,ip22,ip23},
+                                 {ip31,ip32,ip33}  };*/
+
+  Light light11 = new Light(ip11);  Light light12 = new Light(ip12);  Light light13 = new Light(ip13);
+  Light light21 = new Light(ip21);  Light light22 = new Light(ip22);  Light light23 = new Light(ip23);
+  Light light31 = new Light(ip31);  Light light32 = new Light(ip32);  Light light33 = new Light(ip33);
+
+  Light[][] lights = {{light11,light12,light13},{light21,light22,light23},{light31,light32,light33}};
+
+  LightMatrix(){
+    this.sizeX=3;
+    this.sizeY=3;
+  }
+
+  public boolean setLightPos(Light light, int posX, int posY){
+      if(posX<=sizeX && posY<=sizeY){
+          lights[posX][posY]=light;
+          return true;
+      }
+      return false;
+  }
+
+  public Light getLight(int posX, int posY){
+      if(posX<=sizeX && posY<=sizeY){
+          return lights[posX-1][posY-1];
+      }
+      return new Light("127.0.0.1");
+  }
+
+}

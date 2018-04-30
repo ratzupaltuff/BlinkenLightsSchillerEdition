b703f941a677b36988694d46c1a1aa5c97371099
ratzupaltuff <quarksandstings.vw@gmail.com>
2018-04-30 14:50:42 +0200

    .

--- Blinkenlights_ServerProcessing/android/app/src/main/java/processing/test/blinkenlights_serverprocessing/Blinkenlights_ServerProcessing.java
+++ Blinkenlights_ServerProcessing/android/app/src/main/java/processing/test/blinkenlights_serverprocessing/Blinkenlights_ServerProcessing.java
@@ -1,3 +1,5 @@
+package processing.test.blinkenlights_serverprocessing;
+
 import processing.core.*; 
 import processing.data.*; 
 import processing.event.*; 
@@ -321,12 +323,4 @@ class GUI {
 
 	}
   public void settings() {  size(displayWidth, displayHeight); }
-  static public void main(String[] passedArgs) {
-    String[] appletArgs = new String[] { "Blinkenlights_ServerProcessing" };
-    if (passedArgs != null) {
-      PApplet.main(concat(appletArgs, passedArgs));
-    } else {
-      PApplet.main(appletArgs);
-    }
-  }
 }
